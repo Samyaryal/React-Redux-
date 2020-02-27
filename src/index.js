91c0a014069276ddea57a0f1661a446b92e69a68
Samikshya <samikshyaaryal5@gmail.com>
2020-02-27 14:13:12 +0200

    creating mutiple props and passing into child component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail />
+           <CommentDetail 
+                author = "Sam" 
+                timeAgo="Today at 4:45PM" 
+                commentBlog="Nice post"
+                avatar={faker.image.avatar()}
+            />
+           <CommentDetail 
+                author = "Alex"  
+                timeAgo ="Today at 12:OOAM" 
+                commentBlog="good luck with it"
+                avatar={faker.image.avatar()}
+            />
+           <CommentDetail 
+                author = "Jane" 
+                timeAgo=" at 2:00PM" 
+                commentBlog="Great job, keep it up"
+                avatar={faker.image.avatar()}
+            />
           
         </div>
     )
